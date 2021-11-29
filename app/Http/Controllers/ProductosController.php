3e9a6d9655d1e28e0c67aaaff8a0186c634f06a7
bri4nxdd <bryanaranda@gmail.com>
2021-11-29 10:56:47 -0300

    prueba

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Producto;
+use App\Models\Sucursal;
+
+class ProductosController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function listar(){
+        $productos = Producto::paginate(8);
+        return view('productos.listar')
+        ->with('productos', $productos);
+    }
+
+    public function agregar(){
+        $sucursales = Sucursal::all();
+        return view('productos.agregar')
+        ->with('sucursales', $sucursales);
+    }
+
+    public function guardar(Request $request){
+
+        $validateData = $this->validate($request, [
+            'nombre' => 'required|min:3',
+            'precio' => 'integer',
+            'sucursal' => 'required'
+        ]);        
+        $producto = new Producto();
+        $producto->nombre=$request->input('nombre');
+        $producto->precio=$request->input('precio');
+        $producto->sucursal_id=$request->input('sucursal');
+
+
+        //cargar Imagen
+        $imagen = $request->file('imagen');
+        if($imagen){
+            $imagen_path=time().'-'.$imagen->getClientOriginalName();
+            \Storage::disk('imagenes')->put($imagen_path, \File::get($imagen));
+            $producto->imagen=$imagen_path;
+        }
+
+        $producto->save();
+
+        return $this->listar();
+    }
+
+    public function getImagen($filename){
+        $file = \Storage::disk('imagenes')->get($filename);
+        return new Response($file,200);
+    }
+
+    public function deleteProducto($id){
+        $producto = Producto::find($id);
+        if($producto){
+            //Eliminar Imagen
+            \Storage::disk('imagenes')->delete($producto->imagen);
+            $producto->delete();
+            $message="Producto Eliminado Correctamente";
+        }else{
+            $message="El producto no fue eliminado";
+        }
+
+        $productos = Producto::paginate(8);
+        return view('productos.listar')
+        ->with(
+            array(
+                'message' => $message,
+                'productos' => $productos
+            ));
+    }
+
+
+    public function agregarSu(){
+       
+        return view('productos.agregarSu');
+    }
+    public function guardarSucursal(Request $request){
+
+        $validateData = $this->validate($request, [
+            'nombre' => 'required|min:3'
+        ]);        
+        $sucursal = new Sucursal();
+        $sucursal->nombre=$request->input('nombre');
+        $sucursal->save();
+
+        return $this->listar();
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/listar', [App\Http\Controllers\ProductosController::class, 'listar'])->name('listarProductos');
+Route::get('/agregar', [App\Http\Controllers\ProductosController::class, 'agregar'])->name('agregarProductos');
+Route::get('/agregarSu', [App\Http\Controllers\ProductosController::class, 'agregarSu'])->name('agregarSucursal');
+
+Route::post('/guardarProducto', [App\Http\Controllers\ProductosController::class, 'guardar']);
+Route::post('/guardarSucursal', [App\Http\Controllers\ProductosController::class, 'guardarSucursal']);
+
+Route::get('/miniatura/{filename}', [App\Http\Controllers\ProductosController::class, 'getImagen'])->name('miniatura');
+Route::get('/eliminarProducto/{id}', [App\Http\Controllers\ProductosController::class, 'deleteProducto'])->name('eliminarProducto');
